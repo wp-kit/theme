a3eacccef6b67f2054ffe46a236a803b8e8073f6
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-08-22 09:57:18 +0100

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,13 @@
 <?php
     
-    // set your invoked calls here
+    // Set your invoked calls here
     
+    /**
+     * First we invoke the main AppController using invoke() helper
+     *
+     * @var $callback ( string / array / callable )
+     * @var $hook ( string )
+     * @var $condition ( string / callable )
+     * @var $priority ( int )
+     */
     invoke( 'AppController' );
