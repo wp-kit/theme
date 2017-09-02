129e25c04cf698705876db781aa2933969187db8
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-02 07:51:50 +0100

    Added test cases

---
+++ resources/tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+abstract class TestCase extends WPKit\Testing\TestCase
+{
+	
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+}
