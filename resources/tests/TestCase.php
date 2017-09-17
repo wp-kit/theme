794478176bcc36a0e496f6482540007165c542fe
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-17 16:10:47 +0100

    Update TestCase.php

--- resources/tests/TestCase.php
+++ resources/tests/TestCase.php
@@ -7,11 +7,18 @@ use PHPUnit_Framework_TestCase;
 abstract class TestCase extends PHPUnit_Framework_TestCase
 {
 	
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
+	/**
+	* The base URL to use while testing the application.
+	*
+	* @var string
+	*/
+	protected $baseUrl = 'http://localhost';
+	
+	protected function setUp() {
+	
+		// Load the basics part of WordPress.
+		require_once ABSPATH . '/wp-settings.php';
+		
+	}
 
 }
