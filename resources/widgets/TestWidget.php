abadd7dc683203d43c87c3b28f16798d241bfed2
Terence O'Donoghue <terence@creativelittledots.co.uk>
2018-02-23 16:40:50 +0000

    Update TestWidget.php

--- resources/widgets/TestWidget.php
+++ resources/widgets/TestWidget.php
@@ -6,11 +6,11 @@
     
     class TestWidget extends WP_Widget {
         
-        public function __construct() {
+        public function __construct($id_base, $name) {
     		$this->widget_cssclass    = 'test_widget';
     		$this->widget_description = __( 'Test Widget', 'woocommerce' );
-    		$this->widget_id          = 'test_widget';
-    		$this->widget_name        = __( 'Test Widget', 'woocommerce' );
+    		$this->widget_id          = $id_base;
+    		$this->widget_name        = $name;
     		parent::__construct();
     	}
     
