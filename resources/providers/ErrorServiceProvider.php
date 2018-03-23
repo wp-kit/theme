d138ea2dbdee64ac181d4d5f348fe98e0a1b552e
Terence O'Donoghue <terence@creativelittledots.co.uk>
2018-03-23 17:51:17 +0000

    Update ErrorServiceProvider.php

--- resources/providers/ErrorServiceProvider.php
+++ resources/providers/ErrorServiceProvider.php
@@ -15,6 +15,9 @@
 			
 			$whoops = new \Whoops\Run;
 			$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+			$whoops->silenceErrorsInPaths('/.*/', E_USER_NOTICE);
+            		$whoops->silenceErrorsInPaths('/.*/', E_WARNING);
+            		$whoops->silenceErrorsInPaths('/.*/', E_NOTICE);
 			$whoops->register();
 			
 		}
