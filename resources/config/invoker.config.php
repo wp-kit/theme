c8240c4f85bda0185ebf6614323e3885b1f5c645
Terence O'Donoghue <terence@creativelittledots.co.uk>
2020-07-23 22:18:42 +0100

    Update invoker.config.php

--- resources/config/invoker.config.php
+++ resources/config/invoker.config.php
@@ -4,12 +4,24 @@ return [
 
 	/*
     |--------------------------------------------------------------------------
-    | Invoker
+    | Namespace
     |--------------------------------------------------------------------------
     |
     | Tell the Invoker how to find controllers within namespace
     |
     */
     'namespace' => 'Theme\Controllers',
+	
+	/*
+    |--------------------------------------------------------------------------
+    | Register / Boot Methods
+    |--------------------------------------------------------------------------
+    |
+    | Tell the Invoker which methods to call on Controllers for Register and Boot
+    |
+    */
+    'register' => 'register',
+	
+    'boot' => 'boot'
 
 ];
