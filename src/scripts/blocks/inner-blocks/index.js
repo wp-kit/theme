b6e760a3657f07111f8b7cf51562a880583804c5
Terence O'Donoghue <terence@creativelittledots.co.uk>
2020-08-24 22:18:26 +0100

    Update index.js

--- src/scripts/blocks/inner-blocks/index.js
+++ src/scripts/blocks/inner-blocks/index.js
@@ -4,11 +4,15 @@ import moveInnerBlocks from './moveInnerBlocks';
 
 const { addFilter } = wp.hooks
 
-const blocks = acf.data.blockTypes.filter(block => block.has_inner_blocks)
+acf.addAction('prepare', () => {
+	
+	const blocks = acf.data.blockTypes.filter(block => block.has_inner_blocks)
 
-blocks.forEach(block => {
-	addFilter("editor.BlockEdit", `with-inner-blocks/${block.name}`, editWithInnerBlocks(block.name, block.inner_block_params))
-	addFilter("blocks.getSaveElement",  `with-inner-blocks/${block.name}`, saveWithInnerBlocks(block.name))
-	acf.addAction( `render_block_preview/type=${block.name.replace('acf/', '')}`, (preview, attributes) => moveInnerBlocks(preview, attributes, block) )
+	blocks.forEach(block => {
+		addFilter("editor.BlockEdit", `with-inner-blocks/${block.name}`, editWithInnerBlocks(block.name, block.inner_block_params))
+		addFilter("blocks.getSaveElement",  `with-inner-blocks/${block.name}`, saveWithInnerBlocks(block.name))
+		acf.addAction( `render_block_preview/type=${block.name.replace('acf/', '')}`, (preview, attributes) => moveInnerBlocks(preview, attributes, block) )
+	})
+	
 })
 
